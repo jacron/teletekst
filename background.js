33fcdf320b11fa70584366c4674f2b7cc734ebf1
jhsm croonen <info@jcroonen.nl>
2020-01-09 09:10:17 +0100

    display time in title - after reload

--- background.js
+++ background.js
@@ -1,19 +1,28 @@
 const config = {
+    /** load page 101, nieuwsoverzicht */
     teletekstUrl: 'https://nos.nl/teletekst#101_01',
 };
 
 chrome.browserAction.onClicked.addListener(function() {
+    let tTabId;
     chrome.windows.create({
         url: config.teletekstUrl,
         type: 'popup',
         width: 600,
-        height: 700
+        height: 600
     }, win => {
         console.log('win', win);
+        tTabId = win.tabs[0].id;
         chrome.tabs.executeScript(win.tabs[0].id, {
             file: 'scroll.js'
-        }, () => {
-
-        });
+        }, () => {});
+    });
+    /** relad script after refresh of window */
+    chrome.tabs.onUpdated.addListener((tabId, info) => {
+        if (tabId === tTabId) {
+            chrome.tabs.executeScript(tabId, {
+                file: 'scroll.js'
+            }, () => {});
+        }
     });
 });
--- scroll.js
+++ scroll.js
@@ -1,1 +1,11 @@
-window.scrollTo(0, 216);
+/** dit script wordt uitgevoerd door chrome.
+ * dus niet vanzelf telkens als je een refresh doet */
+function setTitleWithTime() {
+    const now = new Date();
+    document.title = 'NOS Teletekst - ' +
+        now.getHours() + ':' +
+        now.getMinutes();
+}
+
+window.scrollTo(0, 294);
+setTitleWithTime();

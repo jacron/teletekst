13e4c6c2a9543fd225e5c45be9b96d117779d10f
jhsm croonen <info@jcroonen.nl>
2020-01-19 01:27:19 +0100

    icons added, multiple popups prevented

--- background.js
+++ background.js
@@ -3,26 +3,45 @@ const config = {
     teletekstUrl: 'https://nos.nl/teletekst#101_01',
 };
 
+let winId = null;
+
 chrome.browserAction.onClicked.addListener(function() {
-    let tTabId;
-    chrome.windows.create({
-        url: config.teletekstUrl,
-        type: 'popup',
-        width: 600,
-        height: 600
-    }, win => {
-        console.log('win', win);
-        tTabId = win.tabs[0].id;
-        chrome.tabs.executeScript(win.tabs[0].id, {
-            file: 'scroll.js'
-        }, () => {});
-    });
-    /** relad script after refresh of window */
-    chrome.tabs.onUpdated.addListener((tabId, info) => {
-        if (tabId === tTabId) {
-            chrome.tabs.executeScript(tabId, {
+    if (winId === null) {
+        /** prevent multiple popups */
+        let tTabId;
+        chrome.windows.create({
+            url: config.teletekstUrl,
+            type: 'popup',
+            width: 600,
+            height: 600,
+            top: 10
+        }, win => {
+            winId = win.id;
+            tTabId = win.tabs[0].id;
+            chrome.tabs.executeScript(win.tabs[0].id, {
                 file: 'scroll.js'
-            }, () => {});
-        }
-    });
+            }, () => {
+            });
+        });
+    } else {
+        chrome.windows.update(winId, {
+            focused: true
+        })
+    }
+    /** reload script after window refresh */
+    // chrome.tabs.onUpdated.addListener((tabId, info) => {
+    //     if (tabId === tTabId) {
+    //         chrome.tabs.executeScript(tabId, {
+    //             file: 'scroll.js'
+    //         }, () => {});
+    //     }
+    // });
 });
+
+chrome.windows.onRemoved.addListener(windowId => {
+    if (windowId === winId) {
+        winId = null;
+        console.log('the popup closed');
+    }
+});
+

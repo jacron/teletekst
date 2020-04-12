0c0ecff4e2f32fb2c052d79e7f75dcb63cf1c561
jhsm croonen <info@jcroonen.nl>
2020-04-12 09:38:59 +0200

    scroll as contentscript

--- background.js
+++ background.js
@@ -8,34 +8,18 @@ let winId = null;
 chrome.browserAction.onClicked.addListener(function() {
     if (winId === null) {
         /** prevent multiple popups */
-        let tTabId;
         chrome.windows.create({
             url: config.teletekstUrl,
             type: 'popup',
             width: 600,
             height: 600,
             top: 10
-        }, win => {
-            winId = win.id;
-            tTabId = win.tabs[0].id;
-            chrome.tabs.executeScript(win.tabs[0].id, {
-                file: 'scroll.js'
-            }, () => {
-            });
-        });
+        }, win => { });
     } else {
         chrome.windows.update(winId, {
             focused: true
         })
     }
-    /** reload script after window refresh */
-    // chrome.tabs.onUpdated.addListener((tabId, info) => {
-    //     if (tabId === tTabId) {
-    //         chrome.tabs.executeScript(tabId, {
-    //             file: 'scroll.js'
-    //         }, () => {});
-    //     }
-    // });
 });
 
 chrome.windows.onRemoved.addListener(windowId => {

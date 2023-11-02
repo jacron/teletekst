4549f531d9320016a8a9e249d32086e2cc690ebc
jan croonen <info@jcroonen.nl>
2023-11-02 05:46:12 +0100

    Update scroll.js
    vervang link naar sport door link naar weer

--- teletekst/scroll.js
+++ teletekst/scroll.js
@@ -4,6 +4,12 @@ function setTitleWithTime() {
         now.getHours() + ':' +
         now.getMinutes();
 }
+function hackWeer() {
+    const linkSport = document.getElementById('fastText4Blue');
+    linkSport.textContent = ' weer ';
+    linkSport.href = '/webplus?p=702';
+}
 console.log('in scroll.js...');
 window.scrollTo(0, 60);
 setTitleWithTime();
+hackWeer();

0e046ba4cdb02e9189569b635e05f440586fa6b0
jan croonen <info@jcroonen.nl>
2023-11-02 09:33:28 +0100

    Update scroll.js
    alle vier links vervangen, weerplaza klikbaar

--- teletekst/scroll.js
+++ teletekst/scroll.js
@@ -4,12 +4,38 @@ function setTitleWithTime() {
         now.getHours() + ':' +
         now.getMinutes();
 }
-function hackWeer() {
-    const linkSport = document.getElementById('fastText4Blue');
-    linkSport.textContent = ' weer ';
-    linkSport.href = '/webplus?p=702';
+function hackLinks() {
+    const hacks = [
+        ['fastText1Red', ' nieuws ', 101],
+        ['fastText2Green', ' actualiteit ', 220],
+        ['fastText3Yellow', ' documentaire ', 228],
+        ['fastText4Blue', ' weer ', 702],
+    ]
+    for (const hack of hacks) {
+        const link = document.getElementById(hack[0]);
+        if (link) {
+            link.textContent = hack[1];
+            link.href = '/webplus?p=' + hack[2];
+        } else {
+            console.log('unknown id', hack[0]);
+        }
+    }
 }
-console.log('in scroll.js...');
+function weerplaza() {
+    const spans = document.getElementsByTagName('span');
+    for (let span of spans) {
+        const text = span.textContent;
+        if (text.indexOf('www.weerplaza.nl') !== -1) {
+            console.log('weerplaza found')
+            const anchor = document.createElement('a');
+            anchor.textContent = text;
+            span.textContent = '';
+            anchor.href = 'https://www.weerplaza.nl';
+            span.appendChild(anchor);
+        }
+    }
+}
 window.scrollTo(0, 60);
 setTitleWithTime();
-hackWeer();
+hackLinks();
+weerplaza();

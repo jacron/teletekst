5d51673120b1ee53fabd6f7b88efa55d2b4c303c
jan croonen <info@jcroonen.nl>
2024-04-01 23:17:03 +0200

    handle submit (goto 220 etc)

--- teletekst/popup/popup.js
+++ teletekst/popup/popup.js
@@ -1,9 +1,10 @@
 const teletekstHome = 'https://teletekst-data.nos.nl';
 const teletekstStart = teletekstHome + '/webplus/?p=101-1';  // https://nos.nl/teletekst#101_01';
+const nativeScriptSrc = 'teletekst-txt.js';
 
+/* followLink() is migrated from teletekst-txt.js, and customized */
 function followLink(link) {
     const url = link.getAttribute('href'); // attribuut is niet geprefixed, zoals .href wel
-    console.log(url)
     if (url && url.length > 0) {
         init(teletekstHome + url);
     }
@@ -28,27 +29,63 @@ function adjustLinks() {
 }
 
 function injectScript(container) {
-    const src = "teletekst-txt.min.js";
     const scriptElement = document.createElement('script');
-    scriptElement.setAttribute('src', src);
+    scriptElement.setAttribute('src', nativeScriptSrc);
     container.appendChild(scriptElement);
 }
 
 function keydownListener(e) {
-    switch (e.key) {
-        case 'F1':
-            document.getElementById('fastText1Red').click();
-            break;
-        case 'F2':
-            document.getElementById('fastText2Green').click();
-            break;
-        case 'F3':
-            document.getElementById('fastText3Yellow').click();
-            break;
-        case 'F4':
-            document.getElementById('fastText4Blue').click();
-            break;
+    const bindings = [
+        ['F1', 'fastText1Red'],
+        ['F2', 'fastText2Green'],
+        ['F3', 'fastText3Yellow'],
+        ['F4', 'fastText4Blue']
+    ]
+    for (let binding of bindings) {
+        const [key, id] = binding;
+        if (key === e.key) {
+            document.getElementById(id).click();
         }
+    }
+}
+
+function makeTextToLink(span, url) {
+    const text = span.textContent;
+    if (text.indexOf(url) !== -1) {
+        const anchor = document.createElement('a');
+        anchor.textContent = text;
+        span.textContent = '';
+        anchor.href = 'https://' + url;
+        anchor.target = '_blank';
+        span.appendChild(anchor);
+    }
+}
+
+function makeLinks() {
+    const spans = document.getElementsByTagName('span');
+    for (let span of spans) {
+        makeTextToLink(span, 'www.weerplaza.nl');
+        makeTextToLink(span, 'www.nos.nl')
+    }
+}
+
+function hideControls() {
+    document.querySelector('.font-control').style.display = 'none';
+}
+
+function handleSubmit() {
+    const form = document.querySelector('form');
+    const input = form.querySelector('input[type=tel]')
+    form.addEventListener('submit', e => {
+        const inputValue = input.value;
+        if (inputValue.length && inputValue.length === 3) {
+            const url = teletekstHome + '/webplus/?p=' + inputValue;
+            console.log(url)
+            init(url);
+        }
+        e.preventDefault();
+        return true;
+    })
 }
 
 function inject(text) {
@@ -58,8 +95,10 @@ function inject(text) {
     container.innerHTML = HTMLDocument.body.innerHTML;
     injectScript(container);
     adjustLinks();
-    document.querySelector('.font-control').style.display = 'none';
+    hideControls();
     container.addEventListener('keydown', keydownListener);
+    handleSubmit();
+    makeLinks();
 }
 
 function init(url) {
---
+++ teletekst/popup/teletekst-txt.js
@@ -0,0 +1,48 @@
+"use strict";
+(function () {
+    /* getCookie() disabled: don't use Cookies (don't change our ideal font size) */
+    /* followLink() is migrated to popup.js, and customized */
+    document.onkeydown = function (event) {
+        event = event || window.event;
+        handleKeyInput(event);
+    };
+    document.getElementById('navi').focus();
+
+    function handleKeyInput(ev) {
+        let keyCode = ev.keyCode || ev.which;
+        let key = {left: 37, up: 38, right: 39, down: 40, pageDown: 34, pageUp: 33};
+        if (keyCode >= 48 && keyCode <= 57) {
+            document.getElementById('navi').focus();
+            return;
+        }
+        const btns_pager = getAllElementsWithAttribute('data-pager');
+        switch (keyCode) {
+            case key.left:
+            case key.pageDown:
+                followLink(btns_pager[0]);
+                break;
+            case key.right:
+            case key.pageUp:
+                followLink(btns_pager[3]);
+                break;
+            case key.up:
+                followLink(btns_pager[1]);
+                break;
+            case key.down:
+                followLink(btns_pager[2]);
+                break;
+        }
+        return;
+    }
+
+    function getAllElementsWithAttribute(attribute) {
+        let matchingElements = [], allElements = document.getElementsByTagName('*'), len = allElements.length;
+        for (let i = 0; i < len; i++) {
+            if (allElements[i].getAttribute(attribute)) {
+                matchingElements.push(allElements[i]);
+            }
+        }
+        return matchingElements;
+    }
+
+})();

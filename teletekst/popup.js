681356639afba51f91d5b9a509694510c32994ce
jan croonen <info@jcroonen.nl>
2024-04-01 14:42:12 +0200

    pijltjestoetsen weer werkend gekregen

--- teletekst/popup.js
+++ teletekst/popup.js
@@ -1,6 +1,13 @@
 const teletekstHome = 'https://teletekst-data.nos.nl';
 const teletekstStart = teletekstHome + '/webplus/?p=101-1';  // https://nos.nl/teletekst#101_01';
 
+function followLink(link) {
+    const url = link.getAttribute('href');
+    if (url && url.length > 0) {
+        init(teletekstHome + url);
+    }
+}
+
 function adjustOneLink(link) {
     const href = link.getAttribute('href');
     if (href && href.startsWith('/')) {
@@ -18,12 +25,22 @@ function adjustLinks() {
         adjustOneLink(link)
     }
 }
+
+function injectScript(container) {
+    const src = "teletekst-txt.min.js";
+    const scriptElement = document.createElement('script');
+    scriptElement.setAttribute('src', src);
+    container.appendChild(scriptElement);
+}
+
 function inject(text) {
     const parser = new DOMParser();
     const HTMLDocument = parser.parseFromString(text, 'text/html');
     const container = document.getElementById('container');
     container.innerHTML = HTMLDocument.body.innerHTML;
+    injectScript(container);
     adjustLinks();
+    document.querySelector('.font-control').style.display = 'none';
 }
 
 function init(url) {

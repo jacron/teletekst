bf25da3e7f9af1e87ada0f3ecc70cea1ef472f3f
jan croonen <info@jcroonen.nl>
2024-04-02 10:24:15 +0200

    backbutton werkend gemaakt

--- teletekst/popup/popup.js
+++ teletekst/popup/popup.js
@@ -1,6 +1,9 @@
 const teletekstHome = 'https://teletekst-data.nos.nl';
 const teletekstStart = teletekstHome + '/webplus/?p=101-1';  // https://nos.nl/teletekst#101_01';
+const teletekstPagina = teletekstHome + '/webplus/?p=';  // https://nos.nl/teletekst#101_01';
 const nativeScriptSrc = 'teletekst-txt.js';
+const KEY_URL_HISTORY = 'url_history';
+const HISTORY_DELIMITER = '%';
 
 /* followLink() is migrated from teletekst-txt.js, and customized */
 function followLink(link) {
@@ -8,6 +11,30 @@ function followLink(link) {
     if (url && url.length > 0) {
         init(teletekstHome + url);
     }
+}
+
+/* set en getCookie overgenomen uit oorspr. script */
+function setCookie(name, value, days) {
+    let expires = "";
+    if (!days) {
+        days = 1;
+    }
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=", ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return false;
 }
 
 function adjustOneLink(link) {
@@ -98,10 +125,52 @@ function inject(text) {
     hideControls();
     container.addEventListener('keydown', keydownListener);
     handleSubmit();
+    handleBack();
     makeLinks();
 }
 
+function pageFromUrl(url) {
+    const p = url.split('?p=');
+    return p[1];
+}
+
+function urlWithPage(page) {
+    return teletekstPagina + page;
+}
+
+function handleBack() {
+    const backButton = document.querySelector('#navigatie .back');
+    backButton.addEventListener('click', e => {
+        let history = getCookie(KEY_URL_HISTORY);
+        if (history !== '') {
+            const p = history.split(HISTORY_DELIMITER);
+            p.pop();
+            const page = p.pop();
+            if (page) {
+                history = p.join(HISTORY_DELIMITER);
+                setCookie(KEY_URL_HISTORY, history);
+                init(urlWithPage(page));
+            }
+        }
+        e.preventDefault();
+    })
+}
+
+function writeHistory(url) {
+    const page = pageFromUrl(url);
+    let history = getCookie(KEY_URL_HISTORY);
+    if (history !== '') {
+        const p = history.split(HISTORY_DELIMITER);
+        p.push(page)
+        history = p.join(HISTORY_DELIMITER)
+    } else {
+        history = page;
+    }
+    setCookie(KEY_URL_HISTORY, history);
+}
+
 function init(url) {
+    writeHistory(url);
     fetch(url, {mode: 'no-cors'})
         .then(res => res.text())
         .then(text => inject(text))
@@ -109,5 +178,6 @@ function init(url) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    setCookie(KEY_URL_HISTORY, '');
     init(teletekstStart);
 });

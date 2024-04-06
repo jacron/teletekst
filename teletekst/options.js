adb4b227b5d0c4e98a93a15c4b1a4a4ab9d155c2
jan croonen <info@jcroonen.nl>
2024-04-06 08:58:35 +0200

    klaar voor save onderregel data

--- teletekst/options.js
+++ teletekst/options.js
@@ -1,17 +1,25 @@
 import {config} from "./popup/config.js";
 
-const optiesTable = document.getElementById('onderregelOpties');
-const rows = optiesTable.querySelectorAll('tr');
-for (let i = 0; i < config.optionalLinks.length; i++) {
-    const row = rows[i];
-    const link = config.optionalLinks[i];
-    const kolommen = row.querySelectorAll('td');
-    kolommen[0].querySelector('input').value = link[0];
-    kolommen[1].querySelector('input').value = link[1].toString();
+function showOnderregel() {
+    const optiesTable = document.getElementById('onderregelOpties');
+    const rows = optiesTable.querySelectorAll('tr');
+    for (let i = 0; i < config.optionalLinks.length; i++) {
+        const row = rows[i];
+        const link = config.optionalLinks[i];
+        const kolommen = row.querySelectorAll('td');
+        kolommen[0].querySelector('input').value = link[0];
+        kolommen[1].querySelector('input').value = link[1].toString();
+    }
+    let length = 0;
+    for (let i = 0; i < config.optionalLinks.length; i++) {
+        length += config.optionalLinks[i][0].length;
+    }
+    const totalLength = document.getElementById('totalLength');
+    totalLength.textContent = length;
 }
-let length = 0;
-for (let i = 0; i < config.optionalLinks.length; i++) {
-    length += config.optionalLinks[i][0].length;
+
+function save() {
+
 }
-const totalLength = document.getElementById('totalLength');
-totalLength.textContent = length;
+
+document.forms[0].onsubmit = save();
